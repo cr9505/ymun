d581a4ad1c18b2dc27575293cc15762af5b1422e
Graham Kaemmer <graham.kaemmer@yale.edu>
2014-03-04 18:57:55 -0500

    more admin asset fixes

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -154,13 +154,13 @@ ActiveAdmin.setup do |config|
   # and feel.
   #
   # To load a stylesheet:
-  config.register_stylesheet 'chosen.css'
+  # config.register_stylesheet 'chosen.css'
   #
   # You can provide an options hash for more control, which is passed along to stylesheet_link_tag():
   #   config.register_stylesheet 'my_print_stylesheet.css', :media => :print
   #
   # To load a javascript file:
-  config.register_javascript 'chosen.jquery.min.js'
+  # config.register_javascript 'chosen.jquery.min.js'
 
 
   # == CSV options
