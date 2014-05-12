9e64a4b6e2249e84abcbc7a9cad825f871291386
Graham Kaemmer <graham.kaemmer@yale.edu>
2014-05-11 23:20:52 -0400

    get ready for branching

--- app/models/option.rb
+++ app/models/option.rb
@@ -22,20 +22,22 @@ class Option < ActiveRecord::Base
   def self.setup
     yield self
 
-    opts = YAML.load_file('config/options/options.yml')
-    opts['options'].each do |opt|
-      if current_opt = where(slug: opt['slug']).first
-        current_opt.name = opt['name']
-        current_opt.value = opt['default'] if current_opt.value.blank?
-        current_opt.save if current_opt.changed?
-      else
-        # create the option with default value
-        new_opt = new
-        new_opt.name = opt['name']
-        new_opt.slug = opt['slug']
-        new_opt.value = opt['default']
-        new_opt.class_name = opt['class_name']
-        new_opt.save
+    if ActiveRecord::Base.connection.table_exists? 'options'
+      opts = YAML.load_file('config/options/options.yml')
+      opts['options'].each do |opt|
+        if current_opt = where(slug: opt['slug']).first
+          current_opt.name = opt['name']
+          current_opt.value = opt['default'] if current_opt.value.blank?
+          current_opt.save if current_opt.changed?
+        else
+          # create the option with default value
+          new_opt = new
+          new_opt.name = opt['name']
+          new_opt.slug = opt['slug']
+          new_opt.value = opt['default']
+          new_opt.class_name = opt['class_name']
+          new_opt.save
+        end
       end
     end
   end
