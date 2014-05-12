ccdc010257cd9b804a6bebb5c466c0b26a0766e7
Graham Kaemmer <graham.kaemmer@yale.edu>
2014-05-12 13:43:02 -0400

    add option to change 'Save' button text

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140310163554) do
+ActiveRecord::Schema.define(version: 20140512173751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(version: 20140310163554) do
   create_table "delegation_pages", force: true do |t|
     t.string  "name"
     t.integer "step"
+    t.string  "save_button"
   end
 
   create_table "delegations", force: true do |t|
