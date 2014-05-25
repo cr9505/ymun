1319e64db094ed9d3436a2e1a919ed899941478d
Graham Kaemmer <graham.kaemmer@yale.edu>
2014-05-25 13:49:51 +0200

    fix payment.method error by renaming db column

--- app/admin/payment.rb
+++ app/admin/payment.rb
@@ -1,15 +1,19 @@
 ActiveAdmin.register Payment do
-  permit_params :name, :amount, :method, :currency, :state
+  permit_params :name, :amount, :payment_method, :currency, :state
   belongs_to :delegation
+
+  scope :all
+  scope :approved, default: true
 
   form do |f|
     f.form_buffers.last << Arbre::Context.new({}, f.template) do
-      input type: 'hidden', name: 'payment[state]', value: 'approved'
     end
     f.inputs do
       f.input :amount
-      f.input :method, label: 'Type (e.g. Check)'
+      f.input :payment_method, label: 'Type (e.g. Check)'
       f.input :currency, label: 'Currency (usd or eur)'
+      f.input :state, as: :select,
+              collection: options_for_select(['created', 'approved', 'failed', 'canceled', 'expired'], f.object.state.presence || 'approved')
     end
     f.actions
   end
