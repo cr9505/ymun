2bdc00fc06c01c4a0d9dd9d154ac2b54c95986ac
Graham Kaemmer <graham.kaemmer@yale.edu>
2014-09-19 17:14:23 -0400

    begin work on committee selection parsing

---
+++ config/initializers/monkey_patches.rb
@@ -0,0 +1,44 @@
+class Hash
+  def diff(other)
+    (self.keys + other.keys).uniq.inject({}) do |memo, key|
+      unless self[key] == other[key]
+        if self[key].kind_of?(Hash) &&  other[key].kind_of?(Hash)
+          memo[key] = self[key].diff(other[key])
+        else
+          memo[key] = [self[key], other[key]] 
+        end
+      end
+      memo
+    end
+  end
+end
+
+module ActionDispatch
+  module Routing
+    class RoutesProxy
+      def new_admin_user_session_path
+        new_user_session_path
+      end
+
+      def new_advisor_session_path
+        new_user_session_path
+      end
+
+      def new_delegate_session_path
+        new_user_session_path
+      end
+
+      def destroy_admin_user_session_path
+        destroy_user_session_path
+      end
+
+      def destroy_advisor_session_path
+        new_user_session_path
+      end
+
+      def destroy_delegate_session_path
+        new_user_session_path
+      end
+    end
+  end
+end
