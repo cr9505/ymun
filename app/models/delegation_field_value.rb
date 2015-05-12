a2c68b65b7f8568077ff10bdff4ebf756ad8a7ae
cr9505 <cr.9505@gmail.com>
2015-05-12 09:23:41 -0400

    Update delegation_field_value.rb

--- app/models/delegation_field_value.rb
+++ app/models/delegation_field_value.rb
@@ -5,7 +5,7 @@ class DelegationFieldValue < ActiveRecord::Base
   validates_presence_of :value, :if => :required
   validates_numericality_of :value, greater_than_or_equal_to: 0, allow_blank: true, :if => :integer?
   validate :value, :if => :select? do |delegation_field_value|
-    options = delegation_field_value.delegation_field.options.and.split(',')
+    options = delegation_field_value.delegation_field.options.andand.split(',')
     if options
       unless options.include?('other')
         unless options.include?(delegation_field_value.to_value)
